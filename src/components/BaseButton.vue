e960301416bbeae537a52eecb68c5219c87cd575
carvelsaputra <carvel.saputra@gmail.com>
2022-02-19 12:42:57 +0700

    install vee validate and prepare login form

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="button">
+    <slot/>
+  </button>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <BaseInput
+            label="email"
+            type="email"
+            v-model="email"
+            :error="emailError"
+        />
+        <BaseInput
+            label="password"
+            type="password"
+        />
+        <BaseButton
+            type="submit"
+            class="-fill-gradient"
+        >
+        Submit
+        </BaseButton>
+    </form>
+</template>
+<script>
+export default {
+    setup(){
+        function onSubmit(){
+            alert("Submitted")
+        }
+        return{
+            onSubmit
+        }
+    }
+}
+</script>
